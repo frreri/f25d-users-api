b6725727e0c288b9a2701fe945b28d946584ede7
frreri <49623505+frreri@users.noreply.github.com>
2025-03-31 12:24:02 +0200

    shortened names

--- script/main.js
+++ script/main.js
@@ -4,14 +4,14 @@ import { UserApp } from './app.js';
 
 // Running below code in a block to give it a private scope, app will not be reachable directly from console in browser
 {
-  const userContainer = document.getElementById('user-container');
-  const searchInput = document.getElementById('search-input');
+  const container = document.getElementById('user-container');
+  const search = document.getElementById('search-input');
 
   // Instantiating and initializing the app
   const app = new UserApp(
     'https://jsonplaceholder.typicode.com/users',
-    userContainer,
-    searchInput,
+    container,
+    search,
   );
   app.initialize();
 }
